ed0ab1d57916208ed626b6fe364b6b0e0e1ac400
Davide Cappelli <36405345+kappesLab@users.noreply.github.com>
2023-12-05 18:23:10 +0100

    Added one View definition

---
+++ PersonDB/dbo/Views/vwPersonPrimaryPhone.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[vwPersonPrimaryPhone]
+AS 
+select [p].[FirstName], [p].[LastName], [p].[DateOfBirth], [p].[EmailAddress], ph.PhoneNumber
+from dbo.Person p
+left join dbo.PhoneNumber ph on p.id = ph.PersonId and  ph.PrimaryNumber = 1;
