d22f7c461d6ef52155254a510002a21becb8700c
Davide Cappelli <36405345+kappesLab@users.noreply.github.com>
2023-12-06 21:17:56 +0100

    BugFixed on Changed for publish

--- Import_OrderDB/dbo/Tables/Orders.sql
+++ Import_OrderDB/dbo/Tables/Orders.sql
@@ -2,7 +2,7 @@
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
     [PurchaseDate] DATETIME2 (7) NULL,
     [Total]        MONEY         NULL,
-    [PersonId] INT NOT NULL, 
+    [PersonId] INT NULL, 
     PRIMARY KEY CLUSTERED ([Id] ASC), 
     CONSTRAINT [FK_Orders_Person] FOREIGN KEY (PersonId) REFERENCES Person([Id])
 );
