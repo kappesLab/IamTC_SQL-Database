82e9c3467a32d396f477c470152aa1dc05feb758
Davide Cappelli <36405345+kappesLab@users.noreply.github.com>
2023-12-06 20:39:08 +0100

    Added Stored Procedures

---
+++ PersonDB/dbo/StoredProcedures/Location_PeopleVisited.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Location_PeopleVisited]
+	@locationId int
+
+AS
+
+begin
+
+	set nocount on;
+
+	select [p].[FirstName], [p].[LastName], p.EmailAddress
+	from dbo.[Location] l
+	inner join dbo.LocationVisit v on l.Id = v.LocationId
+	inner join dbo.Person p on v.PersonId = p.Id
+	where l.Id = @locationId;
+
+end
---
+++ PersonDB/dbo/StoredProcedures/spPerson_LocationVisited.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spPerson_LocationVisited]
+	@PersonId int
+
+AS
+
+begin
+
+	set nocount on;
+
+	select [l].[LocationName], [l].[Location], [v].VisitDate
+	from dbo.Person p
+	inner join dbo.LocationVisit v on p.Id = v.PersonId
+	inner join dbo.[Location] l on v.LocationId = l.Id
+	where p.Id = @PersonId;
+
+end
