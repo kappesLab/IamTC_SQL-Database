34ba882b55aa8ab696f6fa823619b7dad27b2c0c
Davide Cappelli <36405345+kappesLab@users.noreply.github.com>
2023-12-06 20:39:39 +0100

    Modifed the LOcation Table

--- PersonDB/dbo/Tables/Location.sql
+++ PersonDB/dbo/Tables/Location.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
 	[LocationName] nvarchar(50) not null,
-	[Location] nvarchar(50) not null
+	[Location] nvarchar(50) not null, 
+    [Country] NVARCHAR(50) NOT NULL DEFAULT ""
 )
